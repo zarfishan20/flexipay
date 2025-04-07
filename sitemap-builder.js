c9a647d9ec6438dcb58f21e9cfd9849c25c7046e
Zarfishan Ali <zf.ali@hotmail.com>
2025-04-07 16:03:13 +0100

    sitemap modifications

--- sitemap-builder.js
+++ sitemap-builder.js
@@ -1,11 +1,28 @@
-// sitemap-builder.js
-const { Sitemap } = require('react-router-sitemap');
 const fs = require('fs');
+const path = require('path');
+const { SitemapStream, streamToPromise } = require('sitemap'); // Updated import
 
-function generateSitemap() {
-  return new Sitemap(require('./src/App').default)
-    .build('https://flexipaysystems.com')
-    .save('./public/sitemap.xml');
-}
+// Create a writable stream for the sitemap.xml file
+const sitemapStream = new SitemapStream({ hostname: 'https://flexipaysystems.com' });
 
-generateSitemap();
+const links = [
+  { url: '/', changefreq: 'daily', priority: 0.7 },
+  { url: '/services', changefreq: 'weekly', priority: 0.7 },
+  { url: '/about', changefreq: 'weekly', priority: 0.7 },
+  { url: '/contact', changefreq: 'weekly', priority: 0.7 },
+  { url: '/quote', changefreq: 'weekly', priority: 0.7 },
+  // Add more URLs as needed
+];
+
+// Pipe the sitemapStream to the output file
+streamToPromise(sitemapStream).then((data) => {
+  fs.writeFileSync(path.join(__dirname, 'public', 'sitemap.xml'), data);
+});
+
+// Adding URLs to the sitemap
+links.forEach(link => {
+  sitemapStream.write(link);
+});
+
+// End the stream after writing all the links
+sitemapStream.end();
